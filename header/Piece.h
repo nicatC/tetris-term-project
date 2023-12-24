3e9a1b6050462e11d49070b8a85dee95b72ce4eb
nicatC <nichigaan@hotmail.com>
2023-12-24 14:26:27 +0300

    uptodate version

---
+++ header/Piece.h
@@ -0,0 +1,16 @@
+//
+// Created by nevermind on 12/24/23.
+//
+
+#ifndef TETRIS_TERM_PROJECT_PIECE_H
+#define TETRIS_TERM_PROJECT_PIECE_H
+
+
+
+class Piece {
+
+};
+
+
+
+#endif //TETRIS_TERM_PROJECT_PIECE_H
